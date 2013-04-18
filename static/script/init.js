67b487542c62e3e199c57085f8ca49aaad8aa4df
Kim Hsiao - office <white.xtera@gmail.com>
2013-04-18 16:59:57 +0800

    load the other javascript with ajax through $.when() function, and also remove all the js load on html page

--- static/script/init.js
+++ static/script/init.js
@@ -3,60 +3,64 @@
  * 
  * Auther: Kim Hsiao 
  */
- 
-$.getJSON('/data/menuitem.json', function(d) {
-    $.each(d.items, function(k, v) {
-        //extraData(v, 'menu_root');
-        extraData(v, 'ul_topmenu');
-    });
-    
-	$('div.MainMenu').on('click', function() {
-	    if($(this).hasClass('closeMenu')) {
-	        $('div.MainMenu').each(function(k, v) {
-	            if($(this).hasClass('openMenu')) {
-	                $(this).removeClass('openMenu').addClass('closeMenu').siblings('ul.inactive').slideUp('slow');
-	                /* check and close the other open menu items */
-                    }
-	           });
-	        $(this).removeClass('closeMenu').addClass('openMenu').siblings('ul.inactive').slideDown('slow');
-	        /* open selected closed menu item */
-	        if('undefined' == typeof($('span.maintitle').attr('opt')) || $(this).attr('opt') == $('span.maintitle').attr('opt')) {
-               $('div.borderArrow').show('slow');
-           } else {
-               $('div.borderArrow').hide();
-	           }
-           /* when switch to other menu item, hide arrow item */
-          }
-	});
-	
-	$('div.SubMenu').on('click', function() {
-	    var me = $(this), main;
-	    changeResolution.changeArrow(me).addClass('hidden-phone').fadeIn();
-	    changeResolution.changePopC(me).fadeIn('slow');
-	    
-	    $('div.selectItem').removeClass('selectItem').removeClass('itemHover').addClass('SubMenu');
-	    me.addClass('selectItem');
-	    /* change the style and add arrow */
-	   main = me.parents('ul.inactive').siblings('.MainMenu');
-	   $('span.maintitle').attr('opt', main.attr('opt')).text(main.text()).css('cursor', 'pointer').on('click', function() {
-	       $('div[opt=' + main.attr('opt') + ']').trigger('click');
-	   });
-	   $('span.subtitle').attr('opt', me.attr('opt')).text(me.text()).css('cursor', 'pointer').on('click', function() {
-           $('div[opt=' + main.attr('opt') + ']').trigger('click');
-       });
-       /* add pagenation and also add click to go back original page option */
-	}).on('mouseenter mouseleave', function() {
-	    if(!$(this).hasClass('selectItem')) {
-	       $(this).toggleClass('itemHover').toggleClass('SubMenu');
-	      }
-	});
-	
-	(function() {
-	    var main = $('div.MainMenu')[0];
-	    $(main).trigger('click');
-	    $(main).siblings().find('div:first').addClass('itemHover').trigger('click').removeClass('SubMenu');
-	})();
-	// initial open the first menu item
+$.when(
+    $.getScript('/script/jquery-ui.js'),
+    $.getScript('/script/bootstrap.js'),
+    $.getScript('/script/underscore.js'),
+    $.getScript('/script/backbone.js'),
+    /* load all necessary javascript once */
+    $.getJSON('/data/menuitem.json', function(d) {
+        $.each(d.items, function(k, v) {
+            //extraData(v, 'menu_root');
+            extraData(v, 'ul_topmenu');
+        });
+        
+    	$('div.MainMenu').on('click', function() {
+    	    if($(this).hasClass('closeMenu')) {
+    	        $('div.MainMenu').each(function(k, v) {
+    	            if($(this).hasClass('openMenu')) {
+    	                $(this).removeClass('openMenu').addClass('closeMenu').siblings('ul.inactive').slideUp('slow');
+    	                /* check and close the other open menu items */
+                        }
+    	           });
+    	        $(this).removeClass('closeMenu').addClass('openMenu').siblings('ul.inactive').slideDown('slow');
+    	        /* open selected closed menu item */
+    	        if('undefined' == typeof($('span.maintitle').attr('opt')) || $(this).attr('opt') == $('span.maintitle').attr('opt')) {
+                   $('div.borderArrow').show('slow');
+               } else {
+                   $('div.borderArrow').hide();
+    	           }
+               /* when switch to other menu item, hide arrow item */
+              }
+    	});
+    	
+    	$('div.SubMenu').on('click', function() {
+    	    var me = $(this), main;
+    	    changeResolution.changeArrow(me).addClass('hidden-phone').fadeIn();
+    	    changeResolution.changePopC(me).fadeIn('slow');
+    	    
+    	    $('div.selectItem').removeClass('selectItem').removeClass('itemHover').addClass('SubMenu');
+    	    me.addClass('selectItem');
+    	    /* change the style and add arrow */
+    	   main = me.parents('ul.inactive').siblings('.MainMenu');
+    	   $('span.maintitle').attr('opt', main.attr('opt')).text(main.text()).css('cursor', 'pointer').on('click', function() {
+    	       $('div[opt=' + main.attr('opt') + ']').trigger('click');
+    	   });
+    	   $('span.subtitle').attr('opt', me.attr('opt')).text(me.text()).css('cursor', 'pointer').on('click', function() {
+               $('div[opt=' + main.attr('opt') + ']').trigger('click');
+           });
+           /* add pagenation and also add click to go back original page option */
+    	}).on('mouseenter mouseleave', function() {
+    	    if(!$(this).hasClass('selectItem')) {
+    	       $(this).toggleClass('itemHover').toggleClass('SubMenu');
+    	      }
+    	});
+    	// initial open the first menu item
+    })).done(function() {
+        /* initial and run all the click event */
+        var main = $('div.MainMenu')[0];
+        $(main).trigger('click');
+        $(main).siblings().find('div:first').addClass('itemHover').trigger('click').removeClass('SubMenu');
 });
  
 var extraData = function(d, cls) {
